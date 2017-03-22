b0a776c2744e5290b41327be95155424940f5e43
shangyidong <shangyidong@meituan.com>
2017-03-22 14:47:19 +0800

    first commit

---
+++ script/adblocker.js
@@ -0,0 +1,19 @@
+/**
+ * @author: shangyidong@meituan.com
+ * Date: 2017-03-16
+ * Time: 上午11:37
+ */
+var masterBranchUrl = "";
+var betaBranchName = "";
+var betaBranchUrl = "";
+var host = "";
+var groupName = "";
+var appName = "";
+var url = "";
+
+$(function(){
+    host = window.location.host;
+    console.log(host);
+    jQuery(".b_ad").css("display", "none");
+
+})
