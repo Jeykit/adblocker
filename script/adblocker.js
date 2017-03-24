b1e3fdea1d26529b424ff181ca226848efcad9f5
shangyidong <shangyidong@meituan.com>
2017-03-24 11:12:28 +0800

    update

--- script/adblocker.js
+++ script/adblocker.js
@@ -1,19 +1,40 @@
 /**
  * @author: shangyidong@meituan.com
- * Date: 2017-03-16
+ * Date: 2017-03-22
  * Time: 上午11:37
  */
-var masterBranchUrl = "";
-var betaBranchName = "";
-var betaBranchUrl = "";
 var host = "";
-var groupName = "";
-var appName = "";
-var url = "";
 
 $(function(){
     host = window.location.host;
     console.log(host);
-    jQuery(".b_ad").css("display", "none");
-
+    switch (host) {
+        case "www.bing.com":
+        case "cn.bing.com":
+            //jQuery(".b_ad").css("display", "none");
+            jQuery(".b_ad").remove();
+            break;
+        case "www.baidu.com":
+            fuckBaidu();
+            var title = $('title');
+            title.bind('DOMNodeInserted', function(e) {
+                setTimeout(function(){
+                    fuckBaidu();
+                },50);
+            });
+           break;
+        default:
+            console.log("该网站暂未匹配。。。");
+            break;
+    }
 })
+
+function fuckBaidu() {
+    for(var i = 0; i < $("div").length; i++) {
+        if ($($("div")[i]).attr("style") == "display:block !important;visibility:visible !important") {
+            //$($("div")[i]).css("display", "none");
+            $($("div")[i]).remove();
+            console.log("Fuck baidu " + i + "次");
+        }
+    }
+}
