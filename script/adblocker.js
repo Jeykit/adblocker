200764b5a47c02f38bb9ef9d95ae3055b9a64c75
shangyidong <shangyidong@meituan.com>
2017-08-19 09:57:20 +0800

    修改去除百度广告的规则

--- script/adblocker.js
+++ script/adblocker.js
@@ -23,6 +23,11 @@ $(function(){
                     fuckBaidu();
                 },50);
             });
+            $("#su").click(function () {
+                setTimeout(function(){
+                    fuckBaidu();
+                },50);
+            });
            break;
         default:
             console.log("Adblocker暂未匹配当前网站，请前往https://github.com/umgsai/adblocker反馈。");
@@ -31,11 +36,25 @@ $(function(){
 })
 
 function fuckBaidu() {
+
     for(var i = 0; i < $("div").length; i++) {
+        //display:block !important;visibility:visible !important
         if ($($("div")[i]).attr("style") == "display:block !important;visibility:visible !important") {
             //$($("div")[i]).css("display", "none");
             $($("div")[i]).remove();
             console.log("Fuck baidu " + i + "次");
         }
+    }
+
+    var adObjects = new Array();
+    for (var i = 0; i < $("span").length; i++) {
+        if ($("span")[i].innerHTML === "广告") {
+            //$("span.m").eq(i).parent().parent().remove();
+            adObjects.push($("span").eq(i).parent().parent());
+        }
+    }
+    for (var i = 0; i < adObjects.length; i++) {
+        adObjects[i].remove();
+        console.log("Fuck baidu " + i + "次");
     }
 }
